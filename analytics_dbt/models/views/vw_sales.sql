e0b8a2727bc90bab92d3370c46651ff0d34c8796
Victor Buendia <victorbuendia03@gmail.com>
2023-08-11 11:58:19 -0300

    First view created

---
+++ analytics_dbt/models/views/vw_sales.sql
@@ -0,0 +1,22 @@
+WITH vendas AS
+(
+	SELECT
+		salesid AS id_venda
+		, listid AS id_lista
+		, sellerid AS id_vendedor
+		, buyerid AS id_cliente
+		, eventid AS id_evento
+		, dateid AS id_data
+		, qtysold AS quantidade_vendida
+		, pricepaid AS preco_pago
+		, commission AS comissao_vendedor
+		, saletime AS venda_timestamp
+        , TO_CHAR(saletime, 'DD') AS dia_venda
+		, TO_CHAR(saletime, 'MM') AS mes_venda
+		, TO_CHAR(saletime, 'YYYY') AS ano_venda
+        , saletime - INTERVAL '3 HOUR' AS venda_brt
+	FROM
+		tickit.sales
+)
+
+SELECT * FROM vendas
